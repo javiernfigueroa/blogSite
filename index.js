55a20d5b9c1fff41aab3f0e583c892731b3d0136
Javier Figueroa <109968115+javiernfigueroa@users.noreply.github.com>
2023-04-21 10:47:16 -0400

    update

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const BASE_URL = 'https://apis.scrimba.com/jsonplaceholder'
-let endPoint = '/posts'
+const BASE_URL = "https://apis.scrimba.com/jsonplaceholder";
+let endPoint = "/posts";
 
-fetch(BASE_URL+endPoint, {method: 'GET'})
-.then(response => response.json())
-.then(data => {
-    const postsArr = data.slice(0, 5)
-    console.log(postsArr)
-})
+function renderPost() {
+  fetch(BASE_URL + endPoint, { method: "GET" })
+    .then((response) => response.json())
+    .then((data) => {
+      const postsArr = data.slice(0, 5);
+      let html = "";
+      for (let post of postsArr) {
+        html += `
+    <h3>${post.title}</h3>
+    <p>${post.body}</p>
+    <hr/>
+    `;
+      }
+
+      document.getElementById("blog-list").innerHTML = html;
+    });
+}
+
+renderPost();
